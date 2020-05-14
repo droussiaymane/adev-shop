77ee5c925c331603d2b7c5b18efb08a8becca2ee
aymanedroussi <60798100+aymanedroussi@users.noreply.github.com>
2020-05-14 16:50:07 +0000

    Update urls.py

--- adev-shopping/urls.py
+++ adev-shopping/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler404
 
-
+handler404= 'core.views.views_404'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,7 +29,6 @@ urlpatterns = [
 
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
   
