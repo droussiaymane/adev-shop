9406e13ce0cdee4d45d14d03e53921f3c2b45af5
aymanedroussi <60798100+aymanedroussi@users.noreply.github.com>
2020-05-14 16:05:35 +0000

    Update settings.py

--- adev-shopping/settings.py
+++ adev-shopping/settings.py
@@ -164,5 +164,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT= 587
 EMAIL_HOST_USER='adev.off@gmail.com'
-EMAIL_HOST_PASSWORD="adev154238"
+EMAIL_HOST_PASSWORD="adev15423815"
 EMAIL_USE_TLS=True
